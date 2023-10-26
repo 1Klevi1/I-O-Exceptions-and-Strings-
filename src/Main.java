08ccaac92c26017f70c444a910ae230c55323a29
HeatOn <95578244+1Klevi1@users.noreply.github.com>
2023-10-26 12:57:37 +0100

    commented 3 lines that we dont need for now

--- src/Main.java
+++ src/Main.java
@@ -7,9 +7,9 @@ public class Main {
         Collection c = new Collection();
         c.readFromFile("people-2.txt");
         System.out.println(c.searchPeople(4));
-        System.out.println(c.fastMinAge());
-        System.out.println(c.fastMaxAge());
-        System.out.println(c.fastAvgAge());
+//        System.out.println(c.fastMinAge());
+//        System.out.println(c.fastMaxAge());
+//        System.out.println(c.fastAvgAge());
         c.writeToFile("peopleWritten.txt");
 
     }
